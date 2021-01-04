dd6afa7079412567e11f75d849d1378265f9c737
alexangria1992 <alex040892@gmail.com>
2021-01-04 22:18:37 +1300

    How to Create database models in Django

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from .models import Category, Brand, Color, Size, Product
 # Register your models here.
+admin.site.register(Category)
+admin.site.register(Brand)
+admin.site.register(Color)
+admin.site.register(Size)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display=('id', 'title', 'brand', 'color', 'size', 'price', 'status')
+    list_editable=('status',)
+admin.site.register(Product, ProductAdmin)
