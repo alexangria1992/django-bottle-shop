2eca50343461bfb8fcae14b139d5d0456d6697f0
alexangria1992 <alex040892@gmail.com>
2021-01-07 13:57:21 +1300

    fetch data from database

--- main/views.py
+++ main/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from . models import Category
 # Create your views here.
+def home(request):
+    return render(request, 'index.html')
+
+# Category
+def category_list(request):
+    data = Category.objects.all().order_by('-id')
+    return render(request, 'category_list.html', {'data':data})
