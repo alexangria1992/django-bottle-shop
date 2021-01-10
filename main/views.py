e830ab421d02a409b0f9d136d2817fa7f3982217
alexangria1992 <alex040892@gmail.com>
2021-01-10 15:39:02 +1300

    product filters

--- main/views.py
+++ main/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from . models import Category
+from . models import Category, Brand, Product, ProductAttribute
 # Create your views here.
 def home(request):
     return render(request, 'index.html')
@@ -8,3 +8,23 @@ def home(request):
 def category_list(request):
     data = Category.objects.all().order_by('-id')
     return render(request, 'category_list.html', {'data':data})
+
+def brand_list(request):
+    data= Brand.objects.all().order_by('-id')
+    return render(request,'brand_list.html',{'data':data})
+
+def product_list(request):
+	data=Product.objects.all().order_by('-id')
+	cats=Product.objects.distinct().values('category__title','category__id')
+	brands=Product.objects.distinct().values('brand__title','brand__id')
+	colors=ProductAttribute.objects.distinct().values('color__title','color__id','color__color_code')
+	sizes=ProductAttribute.objects.distinct().values('size__title','size__id')
+	return render(request,'product_list.html',
+		{
+			'data':data,
+			'cats':cats,
+			'brands':brands,
+			'colors':colors,
+			'sizes':sizes,
+		}
+		) 
